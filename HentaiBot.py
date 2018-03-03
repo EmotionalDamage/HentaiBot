9f4fbc36423874c151e03f06ef135e9c60320f7a
Hiruna Jayamanne <hirunav2@gmail.com>
2018-03-04 01:33:46 +1300

    Can customise the colour of the embed in the hentaihaven feature Is red by default and must be entered in decimal format (not hexadecimal, but maybe soon)

--- HentaiBot.py
+++ HentaiBot.py
@@ -3,6 +3,7 @@ import feedparser
 from yaml import load, dump
 from json import dumps as jdump
 from requests import post
+from sys import exit as sysexit
 import xml.etree.ElementTree as ET
 BASE_URL = "https://discordapp.com/api"
 
@@ -19,84 +20,91 @@ with open("config.yaml") as file:
     data = load(file)
     token = data["token"]
     channels = data["channels"]
+    num = data["posts"]
+    hentai_haven = data["hentai_haven"]["enabled"]
     hentai_irl = data["hentai_irl"]["enabled"]
     hentai = data["hentai"]["enabled"]
+    if hentai_haven:
+        hentai_haven_num = data["hentai_haven"]["posts"]
+        hentai_haven_channels = data["hentai_haven"]["channels"]
+        hentai_haven_colour = data["hentai_haven"]["embed_colour"]
+        if hentai_haven_colour < 0:
+            hentai_haven_colour = 16711680
+        if len(hentai_haven_channels) == 0:
+            hentai_haven_channels = channels
     if hentai_irl:
         hentai_irl_num = data["hentai_irl"]["posts"]
         hentai_irl_channels = data["hentai_irl"]["channels"]
         if len(hentai_irl_channels) == 0:
             hentai_irl_channels = channels
         if hentai_irl_num < 1:
-            print("Error! The number of hentai_irl posts cannot be less than one")
-            sysexit()
+            hentai_irl_num = num
     if hentai:
         hentai_num = data["hentai"]["posts"]
         hentai_channels = data["hentai"]["channels"]
         if len(hentai_channels) == 0:
             hentai_channels = channels
         if hentai_num < 1:
-            print("The number of hentai posts cannot be less than one")
-            sysexit()
+            hentai_num = num
     if token == "":
         print("Error! No token written in the config.yaml file")
-        sysexit()
-    if len(channels) == 0:
-        print("Error! No channels written in the config.yaml file")
         sysexit()
 with open("last_name", encoding='utf-8') as file:
     end_name = file.read()
 
 
-#Get items from the RSS feed
-x = feedparser.parse("http://hentaihaven.org/feed").entries
-new_items = [] #All the new items will be appended into this list
-counter = 0 #Max limit of 5 items per post
-for i in range(len(x)):
-    if (x[i]['id'] == end_name) or (counter == 5):
-        break
-    else:
-        new_items.append(x[i])
-        counter += 1
-        if i == 0:
-            first_name = x[0]['id']
+#HentaiHaven
+if hentai_haven:
+    #Get items from the RSS feed
+    x = feedparser.parse("http://hentaihaven.org/feed").entries
+    new_items = [] #All the new items will be appended into this list
+    counter = 0 #Max limit of 5 items per post
+    for i in range(len(x)):
+        if (x[i]['id'] == end_name) or (counter == 5):
+            break
+        else:
+            new_items.append(x[i])
+            counter += 1
+            if i == 0:
+                first_name = x[0]['id']
 
-#The RSS Items will be turned into embedded objects
-new_embs = []
-for i in range(len(new_items)):
-    y = new_items[i]
-    desc, image = get_from_summary(y.summary)
-    emb_obj = {
-        "title": y.title,
-        "description": desc,
-        "url": y.link,
-        "color": 16711680,
-        "image": {
-            "url": image
+    #The RSS Items will be turned into embedded objects
+    new_embs = []
+    for i in range(len(new_items)):
+        y = new_items[i]
+        desc, image = get_from_summary(y.summary)
+        emb_obj = {
+            "title": y.title,
+            "description": desc,
+            "url": y.link,
+            "color": hentai_haven_colour,
+            "image": {
+                "url": image
+            }
         }
-    }
-    new_embs.append(emb_obj)
+        new_embs.append(emb_obj)
 
-#Send the messages out to the discord channels
-if len(new_embs) > 0:
-    for ch in channels:
-        for emb in new_embs:
-            sent_msg = post(
-                url = f"{BASE_URL}/channels/{ch}/messages",
-                data = jdump({"content":"New Hentai Release!", "embed":emb}),
-                headers = {"Authorization": f"Bot {token}", "Content-Type":"application/json"}
-            )
-            output = f"To Channel: {ch},   Sent \'{emb['title']}\'"
-            if sent_msg.ok:
-                print("Success!", output)
-            else:
-                print("Error!", output)
+    #Send the messages out to the discord channels
+    if len(new_embs) > 0:
+        for ch in channels:
+            for emb in new_embs:
+                sent_msg = post(
+                    url = f"{BASE_URL}/channels/{ch}/messages",
+                    data = jdump({"content":"New Hentai Release!", "embed":emb}),
+                    headers = {"Authorization": f"Bot {token}", "Content-Type":"application/json"}
+                )
+                output = f"To Channel: {ch},   Sent \'{emb['title']}\'"
+                if sent_msg.ok:
+                    print("Success!", output)
+                else:
+                    print("Error!", output)
 
-    #Update config with the newest item
-    if first_name != end_name:
-        with open("last_name", "w", encoding='utf-8') as file:
-                file.write(first_name)
-else:
-    print("No New Entries")
+        #Update config with the newest item
+        if first_name != end_name:
+            with open("last_name", "w", encoding='utf-8') as file:
+                    file.write(first_name)
+    else:
+        print("No New Entries")
 
 #hentai_irl
 if hentai_irl:
