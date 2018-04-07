7f0b0b51d57f331c67a57f8c8a7f137c726befbd
Hiruna Jayamanne <hirunav2@gmail.com>
2018-04-07 23:43:07 +1200

    Added support for hanime.tv, specifically staging.hanime.tv. This uses web scraping to view hanime.tv . The last_name file has been renamed to last_name.yaml to reflect the fact that is has changed format. Both the HentaiHaven API and the HAnime.tv API will be using the last_name.yaml file. Support for choosing which section you want to use (Recently Uploaded, Trending,  etc.) in HAnime.tv will come soon.

--- HentaiBot.py
+++ HentaiBot.py
@@ -7,12 +7,14 @@ from sys import exit as sysexit
 import xml.etree.ElementTree as ET
 BASE_URL = "https://discordapp.com/api"
 
-# ---------
+
+#  ---------
 def get_from_summary(summary):
     root = ET.fromstring(f"<element>{summary}</element>")
     d = f"{root[1].text}\n\n{root[2].text}"
     i = root[0].attrib["src"]
-    return (d, i)
+    return d, i
+
 
 def tag_filter(tags, summary):
     summary = summary.lower()
@@ -21,16 +23,18 @@ def tag_filter(tags, summary):
         if tag in summary:
             return False
     return True
-# ---------
+#  ---------
+
 
 def start():
-    #Get data from config file
+    # Get data from config file
     with open("config.yaml") as file:
         data = load(file)
         token = str(data["token"])
         channels = data["channels"]
         num = int(data["posts"])
         hentai_haven = data["hentai_haven"]["enabled"]
+        hanime_tv = data["hanime_tv"]["enabled"]
     if hentai_haven:
         hentai_haven_num = int(data["hentai_haven"]["posts"])
         hentai_haven_channels = data["hentai_haven"]["channels"]
@@ -45,31 +49,49 @@ def start():
             hentai_haven_colour = 16711680
         if len(hentai_haven_channels) == 0:
             hentai_haven_channels = channels
+    if hanime_tv:
+        hanime_tv_num = int(data["hanime_tv"]["posts"])
+        hanime_tv_channels = data["hanime_tv"]["channels"]
+        if hanime_tv_num < 1 or hanime_tv is None:
+            hanime_tv_num = num
+        try:
+            hanime_tv_colour = int(data["hanime_tv"]["embed_colour"])
+        except ValueError:
+            hanime_tv_colour = int(data["hanime_tv"]["embed_colour"], 16)
+        if hanime_tv_colour < 0:
+            hanime_tv_channels = int("FF00FF", 16)
+        if len(hanime_tv_channels) == 0:
+            hanime_tv_channels = channels
     if token == "":
         print("Error! No token written in the config.yaml file")
         sysexit()
-    with open("last_name", encoding='utf-8') as file:
-        end_name = file.read()
+    if hentai_haven or hanime_tv:
+        with open("last_name.yaml", encoding='utf-8') as file:
+            end_names = load(file.read())
+            end_name_hh = end_names["hh"]
+            end_name_ha = end_names["ha"]
+    change_name_hh, change_name_ha = False, False
+    first_name_hh, first_name_ha = end_name_hh, end_name_ha
 
 
-    #HentaiHaven
+    # HentaiHaven
     if hentai_haven:
-        #Get items from the RSS feed
+        # Get items from the RSS feed
         x = feedparser.parse("http://hentaihaven.org/feed").entries
-        new_items = [] #All the new items will be appended into this list
-        counter = 0 #Max limit of 5 items per post
-        first_name = ""
+        new_items = []  # All the new items will be appended into this list
+        counter = 0  # Max limit of 5 items per post
+        first_name_hh = ""
         for i in range(len(x)):
-            if (x[i]['id'] == end_name) or (counter == hentai_haven_num):
+            if (x[i]['id'] == end_name_hh) or (counter == hentai_haven_num):
                 break
             else:
                 if tag_filter(hentai_haven_black_list, x[i].summary):
                     new_items.append(x[i])
                     counter += 1
-                    if first_name == "":
-                        first_name = x[0]['id']
+                    if first_name_hh == "":
+                        first_name_hh = x[0]['id']
 
-        #The RSS Items will be turned into embedded objects
+        # The RSS Items will be turned into embedded objects
         new_embs = []
         for i in range(len(new_items)):
             y = new_items[i]
@@ -85,29 +107,29 @@ def start():
             }
             new_embs.append(emb_obj)
 
-        #Send the messages out to the discord channels
+        # Send the messages out to the discord channels
         if len(new_embs) > 0:
             for ch in channels:
                 for emb in new_embs:
                     sent_msg = post(
-                        url = f"{BASE_URL}/channels/{ch}/messages",
-                        data = jdump({"content":"New Hentai Release!", "embed":emb}),
-                        headers = {"Authorization": f"Bot {token}", "Content-Type":"application/json"}
+                        url=f"{BASE_URL}/channels/{ch}/messages",
+                        data=jdump({"content": "New Hentai Release!", "embed": emb}),
+                        headers={"Authorization": f"Bot {token}", "Content-Type": "application/json"}
                     )
                     output = f"To Channel: {ch},    Sent \'{emb['title']}\'"
                     if sent_msg.ok:
                         print("Success!", output)
                     else:
                         print("Error!", output)
+                        print(sent_msg.text)
 
-            #Update config with the newest item
-            if first_name != end_name:
-                with open("last_name", "w", encoding='utf-8') as file:
-                        file.write(first_name)
+            # Update config with the newest item
+            if first_name_hh != end_name_hh:
+                change_name_hh = True
         else:
             print("Info     No New HentaiHaven Entries")
 
-    #Reddit Plugins
+    # Reddit Plugins
     for i in data["reddit"].keys():
         import reddit_api
         subred = data["reddit"][i]
@@ -120,15 +142,55 @@ def start():
         links = reddit_api.go(i, r_num)
         for ch in r_chs:
             sent_msg = post(
-                url = f"{BASE_URL}/channels/{ch}/messages",
-                data = jdump({"content":links}),
-                headers = {"Authorization": f"Bot {token}", "Content-Type":"application/json"}
+                url=f"{BASE_URL}/channels/{ch}/messages",
+                data=jdump({"content": links}),
+                headers={"Authorization": f"Bot {token}", "Content-Type": "application/json"}
             )
             output = f"To Channel: {ch},    Sent {r_num} r/{i} Posts"
             if sent_msg.ok:
                 print(f"Success!", output)
             else:
                 print(f"Error!  ", output)
+
+    # HAnime.tv
+    if hanime_tv:
+        import hanime_tv
+        embeds, first_id = hanime_tv.go(
+            section=hanime_tv.RECENT_UPLOADS,
+            colour=hanime_tv_colour,
+            limit=hanime_tv_num,
+            end_name=end_name_ha
+        )
+        change_name_ha = first_id[0]
+        if change_name_ha:
+            first_name_ha = first_id[1]
+            print(first_name_ha)
+        else:
+            first_name_ha = end_name_ha
+        emb_num = len(embeds)
+        if emb_num == 0:
+            print("Info     No New HAnime.tv Entries")
+        for e in embeds:
+            for ch in hanime_tv_channels:
+                sent_msg = post(
+                    url=f"{BASE_URL}/channels/{ch}/messages",
+                    data=jdump({"content": "New Hentai Release", "embed": e}),
+                    headers={"Authorization": f"Bot {token}", "Content-Type": "application/json"}
+                )
+                output = f"To Channel: {ch},    Sent {emb_num} HAnime.tv Posts"
+                if sent_msg.ok:
+                    print(f"Success!", output)
+                else:
+                    print(f"Error!  ", output)
+                    print(sent_msg.text)
+
+    if change_name_hh or change_name_ha:
+        with open("last_name.yaml", "w", encoding='utf-8') as file:
+            file.write(dump({
+                "hh": first_name_hh,
+                "ha": first_name_ha
+            }))
+
 
 if __name__ == "__main__":
     start()
---
+++ hanime_tv.py
@@ -0,0 +1,107 @@
+# Hanime.tv API ~ Uses web scraping so don't use it too much.
+from requests import get
+from json import loads
+from math import floor
+
+URL = "https://staging.hanime.tv"
+RECENT_UPLOADS = 0
+NEW_RELEASES = 1
+TRENDING = 2
+RANDOM = 3
+
+
+def go(section=RECENT_UPLOADS, limit=5, end_name=0, colour=0):
+    response = get(URL)
+    if response.status_code != 200:
+        return None
+    _, response = response.text.split("__NUXT__=")
+    response, _ = response.split(";")
+    response = loads(response)
+    if 0 > section > 4:
+        section = RECENT_UPLOADS
+    root = response["state"]["data"]["landing"]
+    hentai_list = root["sections"][section]["hentai_video_ids"]
+    video_list = map_list(root["hentai_videos"])
+    first_video = end_name
+    output = []
+    for i in range(len(hentai_list)):
+        if i <= (limit-1):
+            id_ = hentai_list[i]
+            video = video_list[id_]
+            if id_ == end_name:
+                break
+            elif i == 0:
+                first_video = id_
+            output.append({
+                "id": id_,
+                "name": video["name"],
+                "url": f"{URL}/hentai-videos/{video['slug']}",
+                "image": video["cover_url"],
+                "poster": video["poster_url"],
+                "subbed": video["is_hard_subtitled"],
+                "censored": video["is_censored"],
+                "duration": video["duration_in_ms"],
+            })
+        else:
+            break
+    if first_video != end_name:
+        end_video_changed = True
+        print("Changed")
+    else:
+        end_video_changed = False
+    return embed_format(output, colour), (end_video_changed, first_video)
+
+
+def embed_format(videos, embed_colour):
+    COL = 12
+    output = []
+    for v in videos:
+        try:
+            minutes = floor(v["duration"] / 60_000)
+            seconds = floor((v["duration"] % 60_000) / 1_000)
+        except:
+            minutes = "??"
+            seconds = "??"
+        try:
+            desc = "Duration:".ljust(COL) + f"{minutes}:{seconds}\n"
+            desc += "Subbed:".ljust(COL) + str(v["subbed"]) + "\n"
+            desc += "Censored:".ljust(COL) + str(v["censored"])
+        except:
+            desc = ""
+        output.append({
+            "title": v["name"],
+            "description": desc,
+            "url": v["url"],
+            "color": embed_colour,
+            "thumbnail": {
+                "url": v["image"]
+            },
+            "image": {
+                "url": v["poster"]
+            },
+        })
+    return output
+
+
+def map_list(video_list):
+    mapped = {}
+    for i in video_list:
+        mapped[i["id"]] = i
+    return mapped
+
+
+if __name__ == "__main__":
+    result = go()
+    if result is not None:
+        for i in result:
+            print("====================")
+            print(i["name"])
+            print(i["id"])
+            print(i["url"])
+            print(i["image"])
+            print(i["subbed"])
+            print(i["censored"])
+            print(i["duration"])
+            print("====================")
+    else:
+        print(result.content)
